cfa2171050767a4e1dd84cd60022fa30bf34c14b
Andreas Maeder <maederan@phys.ethz.ch>
2021-10-25 18:11:09 +0200

    added float64 pythranized version

--- modesolverpy/_pythran_lib.py
+++ modesolverpy/_pythran_lib.py
@@ -3,6 +3,7 @@ def centered1d(x):
     return (x[1:] + x[:-1]) / 2.
 
 #pythran export centered2d(complex128[][])
+#pythran export centered2d(float64[][])
 def centered2d(x):
     return (x[1:, 1:] + x[1:, :-1] + x[:-1, 1:] + x[:-1, :-1]) / 4.
     
