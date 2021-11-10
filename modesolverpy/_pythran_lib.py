9d6b5ec2a4d640b7425b63ebbc1ee53b0d243463
Andreas Maeder <maederan@phys.ethz.ch>
2021-11-10 15:03:55 +0100

    Added pythran_lib back

---
+++ modesolverpy/_pythran_lib.py
@@ -0,0 +1,9 @@
+#pythran export centered1d(float[])
+def centered1d(x):
+    return (x[1:] + x[:-1]) / 2.
+
+#pythran export centered2d(complex128[][])
+#pythran export centered2d(float64[][])
+def centered2d(x):
+    return (x[1:, 1:] + x[1:, :-1] + x[:-1, 1:] + x[:-1, :-1]) / 4.
+    
